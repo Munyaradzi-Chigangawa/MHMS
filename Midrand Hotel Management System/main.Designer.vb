26f96a6b90bcd051f7d7a9fb745c90dc6dc51674
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-05-06 07:02:10 +0200

    main

---
+++ Midrand Hotel Management System/main.Designer.vb
@@ -0,0 +1,346 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(main))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnaccount = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnTransaction = New System.Windows.Forms.Button()
+        Me.btnCheckIn = New System.Windows.Forms.Button()
+        Me.btnReservation = New System.Windows.Forms.Button()
+        Me.btnGuest = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(-9, -1)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1149, 117)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label2.Location = New System.Drawing.Point(393, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(363, 77)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "MIDRAND"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label1.Location = New System.Drawing.Point(393, 72)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(362, 33)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Hotel Management System"
+        '
+        'btnaccount
+        '
+        Me.btnaccount.BackColor = System.Drawing.Color.Transparent
+        Me.btnaccount.BackgroundImage = CType(resources.GetObject("btnaccount.BackgroundImage"), System.Drawing.Image)
+        Me.btnaccount.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnaccount.FlatAppearance.BorderSize = 0
+        Me.btnaccount.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnaccount.Location = New System.Drawing.Point(522, 535)
+        Me.btnaccount.Name = "btnaccount"
+        Me.btnaccount.Size = New System.Drawing.Size(83, 65)
+        Me.btnaccount.TabIndex = 16
+        Me.btnaccount.UseVisualStyleBackColor = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnTransaction)
+        Me.GroupBox2.Controls.Add(Me.btnCheckIn)
+        Me.GroupBox2.Controls.Add(Me.btnReservation)
+        Me.GroupBox2.Controls.Add(Me.btnGuest)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Font = New System.Drawing.Font("Century", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(59, 161)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(473, 356)
+        Me.GroupBox2.TabIndex = 17
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Transaction"
+        '
+        'btnTransaction
+        '
+        Me.btnTransaction.BackColor = System.Drawing.Color.Transparent
+        Me.btnTransaction.BackgroundImage = CType(resources.GetObject("btnTransaction.BackgroundImage"), System.Drawing.Image)
+        Me.btnTransaction.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnTransaction.Location = New System.Drawing.Point(288, 191)
+        Me.btnTransaction.Name = "btnTransaction"
+        Me.btnTransaction.Size = New System.Drawing.Size(118, 118)
+        Me.btnTransaction.TabIndex = 0
+        Me.btnTransaction.UseVisualStyleBackColor = False
+        '
+        'btnCheckIn
+        '
+        Me.btnCheckIn.BackColor = System.Drawing.Color.Transparent
+        Me.btnCheckIn.BackgroundImage = CType(resources.GetObject("btnCheckIn.BackgroundImage"), System.Drawing.Image)
+        Me.btnCheckIn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnCheckIn.Location = New System.Drawing.Point(288, 37)
+        Me.btnCheckIn.Name = "btnCheckIn"
+        Me.btnCheckIn.Size = New System.Drawing.Size(118, 118)
+        Me.btnCheckIn.TabIndex = 0
+        Me.btnCheckIn.UseVisualStyleBackColor = False
+        '
+        'btnReservation
+        '
+        Me.btnReservation.BackColor = System.Drawing.Color.Transparent
+        Me.btnReservation.BackgroundImage = CType(resources.GetObject("btnReservation.BackgroundImage"), System.Drawing.Image)
+        Me.btnReservation.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnReservation.Location = New System.Drawing.Point(28, 191)
+        Me.btnReservation.Name = "btnReservation"
+        Me.btnReservation.Size = New System.Drawing.Size(118, 118)
+        Me.btnReservation.TabIndex = 0
+        Me.btnReservation.UseVisualStyleBackColor = False
+        '
+        'btnGuest
+        '
+        Me.btnGuest.BackColor = System.Drawing.Color.Transparent
+        Me.btnGuest.BackgroundImage = CType(resources.GetObject("btnGuest.BackgroundImage"), System.Drawing.Image)
+        Me.btnGuest.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnGuest.Location = New System.Drawing.Point(28, 37)
+        Me.btnGuest.Name = "btnGuest"
+        Me.btnGuest.Size = New System.Drawing.Size(118, 118)
+        Me.btnGuest.TabIndex = 0
+        Me.btnGuest.UseVisualStyleBackColor = False
+        '
+        'Label7
+        '
+        Me.Label7.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(283, 312)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(130, 25)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Transaction"
+        '
+        'Label6
+        '
+        Me.Label6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(23, 312)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(130, 25)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Reservation"
+        '
+        'Label5
+        '
+        Me.Label5.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(293, 158)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(100, 25)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Check In"
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(31, 158)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(115, 25)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Add Guest"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Button6)
+        Me.GroupBox1.Controls.Add(Me.Button7)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Font = New System.Drawing.Font("Century", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(595, 161)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(473, 356)
+        Me.GroupBox1.TabIndex = 18
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Monitoring"
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Transparent
+        Me.Button4.BackgroundImage = CType(resources.GetObject("Button4.BackgroundImage"), System.Drawing.Image)
+        Me.Button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button4.Location = New System.Drawing.Point(288, 191)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(118, 118)
+        Me.Button4.TabIndex = 0
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Transparent
+        Me.Button5.BackgroundImage = CType(resources.GetObject("Button5.BackgroundImage"), System.Drawing.Image)
+        Me.Button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button5.Location = New System.Drawing.Point(288, 37)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(118, 118)
+        Me.Button5.TabIndex = 0
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.Transparent
+        Me.Button6.BackgroundImage = CType(resources.GetObject("Button6.BackgroundImage"), System.Drawing.Image)
+        Me.Button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button6.Location = New System.Drawing.Point(28, 191)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(118, 118)
+        Me.Button6.TabIndex = 0
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.Transparent
+        Me.Button7.BackgroundImage = CType(resources.GetObject("Button7.BackgroundImage"), System.Drawing.Image)
+        Me.Button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button7.Location = New System.Drawing.Point(28, 37)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(118, 118)
+        Me.Button7.TabIndex = 0
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(305, 312)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(88, 25)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Reports"
+        '
+        'Label8
+        '
+        Me.Label8.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(23, 312)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(143, 25)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Search Guest"
+        '
+        'Label9
+        '
+        Me.Label9.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(305, 158)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(86, 25)
+        Me.Label9.TabIndex = 7
+        Me.Label9.Text = "History"
+        '
+        'Label10
+        '
+        Me.Label10.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(31, 158)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(113, 25)
+        Me.Label10.TabIndex = 7
+        Me.Label10.Text = "Room List"
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1126, 629)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.btnaccount)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "main"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnaccount As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnGuest As System.Windows.Forms.Button
+    Friend WithEvents btnTransaction As System.Windows.Forms.Button
+    Friend WithEvents btnCheckIn As System.Windows.Forms.Button
+    Friend WithEvents btnReservation As System.Windows.Forms.Button
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+End Class
---
+++ Midrand Hotel Management System/main.vb
@@ -0,0 +1,46 @@
+﻿Public Class main
+
+    Private Sub btnaccount_Click(sender As Object, e As EventArgs) Handles btnaccount.Click
+        If MessageBox.Show("Please confirm shutting down the system!", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) = Windows.Forms.DialogResult.Yes Then
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub btnGuest_Click(sender As Object, e As EventArgs)
+
+        guestinfo.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub btnCheckIn_Click(sender As Object, e As EventArgs) Handles btnCheckIn.Click
+        checkin.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub btnTransaction_Click(sender As Object, e As EventArgs) Handles btnTransaction.Click
+        cashier.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub btnReservation_Click(sender As Object, e As EventArgs) Handles btnReservation.Click
+        reservation.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub btnGuest_Click_1(sender As Object, e As EventArgs) Handles btnGuest.Click
+        guestinfo.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        rooms.ShowDialog()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Search.ShowDialog()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        history.ShowDialog()
+    End Sub
+End Class
