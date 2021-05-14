3a5900a1553c150a643cbbb8c62787f5988424bb
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-05-14 07:07:03 +0200

    checkin

---
+++ Midrand Hotel Management System/checkinlist.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class checkinlist
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Century", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(39, 35)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 16)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Guest list"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(39, 321)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(115, 16)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Check reserve list"
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.AllowUserToAddRows = False
+        Me.DataGridView2.AllowUserToDeleteRows = False
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(39, 349)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.ReadOnly = True
+        Me.DataGridView2.Size = New System.Drawing.Size(1051, 239)
+        Me.DataGridView2.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(944, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(145, 16)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Double click the  name"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(39, 59)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(1051, 239)
+        Me.DataGridView1.TabIndex = 5
+        '
+        'checkinlist
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1126, 629)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "checkinlist"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "checkinlist"
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents DataGridView2 As System.Windows.Forms.DataGridView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+End Class
---
+++ Midrand Hotel Management System/checkinlist.vb
@@ -0,0 +1,101 @@
+﻿Public Class checkinlist
+    Dim cmd As New OleDb.OleDbCommand
+    Dim sql As String
+    Dim da As New OleDb.OleDbDataAdapter
+    Dim dt As New DataTable
+    Dim result As Integer
+    Dim Fname As String
+    Dim Lname As String
+
+    Dim conn As OleDb.OleDbConnection = Myconnection()
+
+    Public Function Myconnection() As OleDb.OleDbConnection
+        Return New OleDb.OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & Application.StartupPath & "\mhms.mdb")
+    End Function
+    Private Sub checkinlist_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dt = New DataTable
+        Try
+            sql = "SELECT * FROM guests"
+            conn.Open()
+            With cmd
+                .CommandText = sql
+                .Connection = conn
+            End With
+
+            da.SelectCommand = cmd
+            da.Fill(dt)
+            DataGridView1.DataSource = dt
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            conn.Close()
+
+
+        End Try
+        dt = New DataTable
+        Try
+            sql = "SELECT * FROM reserve"
+            conn.Open()
+            With cmd
+                .CommandText = sql
+                .Connection = conn
+            End With
+
+            da.SelectCommand = cmd
+            da.Fill(dt)
+            DataGridView2.DataSource = dt
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            conn.Close()
+
+
+        End Try
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        dt = New DataTable
+        GuestId = DataGridView1.CurrentRow.Cells("ID").Value.ToString
+    End Sub
+
+    Private Sub DataGridView1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles DataGridView1.DoubleClick
+
+        'checkin
+        checkin.txtgname.Text = DataGridView1.CurrentRow.Cells("FNAME").Value.ToString()
+        checkin.txtgname.Text &= " " & DataGridView1.CurrentRow.Cells("LNAME").Value.ToString
+        checkin.txtgname.Text &= " " & DataGridView1.CurrentRow.Cells("NATIONALITY").Value.ToString
+
+        'reservation
+        reservation.txtgname1.Text = DataGridView1.CurrentRow.Cells("FNAME").Value.ToString()
+        reservation.txtgname1.Text &= " " & DataGridView1.CurrentRow.Cells("LNAME").Value.ToString
+        reservation.txtgname1.Text &= " " & DataGridView1.CurrentRow.Cells("NATIONALITY").Value.ToString
+        main.BringToFront()
+        Me.Hide()
+    End Sub
+
+    Private Sub DataGridView2_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView2.CellContentClick
+        dt = New DataTable
+        GuestId = DataGridView1.CurrentRow.Cells("ID").Value.ToString
+    End Sub
+
+    Private Sub DataGridView2_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles DataGridView2.DoubleClick
+        dt = New DataTable
+        GuestId = DataGridView2.CurrentRow.Cells("ID").Value.ToString
+        checkin.txtgname.Text &= " " & DataGridView2.CurrentRow.Cells("GUESTNAME").Value.ToString
+        checkin.txtRoom.Text = DataGridView2.CurrentRow.Cells("ROOM_NO").Value.ToString()
+        checkin.rate.Text = DataGridView2.CurrentRow.Cells("ROOM_RATE").Value.ToString()
+        checkin.txttype.Text = DataGridView2.CurrentRow.Cells("ROOM_TYPE").Value.ToString()
+
+        checkin.txtdate.Value = DataGridView2.CurrentRow.Cells("CHECKIN_DATE").Value.ToString()
+        checkin.txtdays.Text = DataGridView2.CurrentRow.Cells("NOOFDAYS").Value.ToString()
+        checkin.txtadults.Text = DataGridView2.CurrentRow.Cells("NOOFADULTS").Value.ToString()
+        checkin.txtChildren.Text = DataGridView2.CurrentRow.Cells("NOOFCHILDREN").Value.ToString()
+        checkin.txtadvance.Text = DataGridView2.CurrentRow.Cells("ADVANCE").Value.ToString()
+
+        Me.Close()
+    End Sub
+End Class
