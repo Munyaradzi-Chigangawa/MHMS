5a5d5a541c80e5417aee3d7bd08654632295a846
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-06-11 07:14:23 +0200

    history

---
+++ Midrand Hotel Management System/history.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class history
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(history))
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(795, 503)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(203, 31)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Cancel Reservation"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(133, 94)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(179, 25)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Search by Name:"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(128, 154)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(870, 343)
+        Me.DataGridView1.TabIndex = 9
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button1.Location = New System.Drawing.Point(728, 112)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(54, 40)
+        Me.Button1.TabIndex = 8
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Century", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(128, 122)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(594, 26)
+        Me.TextBox1.TabIndex = 7
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Check in list", "Check out list", "Reserve list"})
+        Me.ComboBox1.Location = New System.Drawing.Point(788, 121)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(210, 24)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'history
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1126, 629)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "history"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "history"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+End Class
---
+++ Midrand Hotel Management System/history.vb
@@ -0,0 +1,188 @@
+﻿Public Class history
+    Dim cmd As New OleDb.OleDbCommand
+    Dim sql As String
+    Dim da As New OleDb.OleDbDataAdapter
+    Dim dt As New DataTable
+    Dim result As Integer
+
+    Dim conn As OleDb.OleDbConnection = Myconnection()
+
+    Public Function Myconnection() As OleDb.OleDbConnection
+        Return New OleDb.OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & Application.StartupPath & "\mhms.mdb")
+    End Function
+    Private Sub history_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button2.Hide()
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        dt = New DataTable
+        GuestId = DataGridView1.CurrentRow.Cells("ID").Value.ToString
+        checkin.txtgname.Text &= " " & DataGridView1.CurrentRow.Cells("GUESTNAME").Value.ToString
+        checkin.txtRoom.Text = DataGridView1.CurrentRow.Cells("ROOM_NO").Value.ToString()
+        checkin.rate.Text = DataGridView1.CurrentRow.Cells("ROOM_RATE").Value.ToString()
+        checkin.txttype.Text = DataGridView1.CurrentRow.Cells("ROOM_TYPE").Value.ToString()
+
+        checkin.txtdate.Value = DataGridView1.CurrentRow.Cells("CHECKIN_DATE").Value.ToString()
+        checkin.txtdays.Text = DataGridView1.CurrentRow.Cells("NOOFDAYS").Value.ToString()
+        checkin.txtadults.Text = DataGridView1.CurrentRow.Cells("NOOFADULTS").Value.ToString()
+        checkin.txtChildren.Text = DataGridView1.CurrentRow.Cells("NOOFCHILDREN").Value.ToString()
+        checkin.txtadvance.Text = DataGridView1.CurrentRow.Cells("ADVANCE").Value.ToString()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ComboBox1.Text = "Check in list" Then
+
+            dt = New DataTable
+            sql = "Select * from checkin where GUESTNAME LIKE '%" & TextBox1.Text & "%'"
+
+            Try
+
+                con.Open()
+                da = New OleDb.OleDbDataAdapter(Sql, con)
+                da.Fill(dt)
+                DataGridView1.DataSource = dt
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Information)
+            End Try
+
+            con.Close()
+        ElseIf ComboBox1.Text = "Check out list" Then
+            dt = New DataTable
+            sql = "Select * from checkout where GUESTNAME LIKE '%" & TextBox1.Text & "%'"
+
+            Try
+
+                con.Open()
+                da = New OleDb.OleDbDataAdapter(Sql, con)
+                da.Fill(dt)
+                DataGridView1.DataSource = dt
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Information)
+            End Try
+
+            con.Close()
+
+        ElseIf ComboBox1.Text = "Reserve list" Then
+            Button2.Show()
+            dt = New DataTable
+            sql = "Select * from reserve where GUESTNAME LIKE '%" & TextBox1.Text & "%'"
+
+            Try
+
+                con.Open()
+                da = New OleDb.OleDbDataAdapter(Sql, con)
+                da.Fill(dt)
+                DataGridView1.DataSource = dt
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Information)
+            End Try
+
+            con.Close()
+
+        End If
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.Text = "Check in list" Then
+            dt = New DataTable
+            Try
+                sql = "SELECT * FROM checkin"
+                conn.Open()
+                With cmd
+                    .CommandText = sql
+                    .Connection = conn
+                End With
+
+                da.SelectCommand = cmd
+                da.Fill(dt)
+                DataGridView1.DataSource = dt
+
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                conn.Close()
+            End Try
+
+        ElseIf ComboBox1.Text = "Check out list" Then
+            dt = New DataTable
+            Try
+                sql = "SELECT * FROM checkout"
+                conn.Open()
+                With cmd
+                    .CommandText = sql
+                    .Connection = conn
+                End With
+
+                da.SelectCommand = cmd
+                da.Fill(dt)
+                DataGridView1.DataSource = dt
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                conn.Close()
+            End Try
+
+        ElseIf ComboBox1.Text = "Reserve list" Then
+            Button2.Show()
+            dt = New DataTable
+            Try
+                sql = "SELECT * FROM reserve"
+                conn.Open()
+                With cmd
+                    .CommandText = sql
+                    .Connection = conn
+                End With
+
+                da.SelectCommand = cmd
+                da.Fill(dt)
+                DataGridView1.DataSource = dt
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                conn.Close()
+            End Try
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim CancelReservation = MessageBox.Show("Are you want to Cancel Your Reservation", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If CancelReservation = DialogResult.Yes Then
+            con.Open()
+
+            sql = "Update rooms set Status = 'Available' where rooms= '" & DataGridView1.CurrentRow.Cells("ROOM_NO").Value & "'"
+
+            Try
+
+                da = New OleDb.OleDbDataAdapter(sql, con)
+                da.Fill(dt)
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Information)
+            End Try
+
+            da.Dispose()
+
+            dt = New DataTable
+            sql = "Delete * from reserve where ROOM_NO= '" & DataGridView1.CurrentRow.Cells("ROOM_NO").Value & "'"
+
+            Try
+
+                da = New OleDb.OleDbDataAdapter(sql, con)
+                da.Fill(dt)
+                DataGridView1.DataSource = dt
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Information)
+            End Try
+
+
+
+            con.Close()
+        Else : CancelReservation = DialogResult.No
+
+        End If
+
+
+    End Sub
+End Class
